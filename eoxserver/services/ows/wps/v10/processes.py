08b40f4c5d3f64ed91f00480ae4d555f9279277e
Daniel Santillan <daniel.santillan@eox.at>
2014-05-09 15:26:27 +0200

    Began implementation of a new process that creates difference image of two coverages

--- eoxserver/services/ows/wps/v10/processes.py
+++ eoxserver/services/ows/wps/v10/processes.py
@@ -1,18 +1,22 @@
 
 
-
+import os 
+import os.path
+from uuid import uuid4
 from datetime import datetime
 import struct
 import csv
 from StringIO import StringIO
 
+import numpy as np 
 
-from django.contrib.gis.geos import Point, MultiPoint
+from django.contrib.gis.geos import Point, MultiPoint, Polygon
 from django.contrib.gis.gdal import SpatialReference
 from django.db.models import Q
 
 from eoxserver.core import Component, ExtensionPoint, implements
 from eoxserver.core.util.timetools import isoformat
+from eoxserver.core.util.rect import Rect
 from eoxserver.contrib import gdal
 from eoxserver.backends.access import connect
 from eoxserver.services.ows.wps.interfaces import ProcessInterface
@@ -20,13 +24,16 @@ from eoxserver.services.ows.wps.parameters import LiteralData, ComplexData
 from eoxserver.resources.coverages import models
 from eoxserver.services.subset import Subsets, Trim
 
+import logging
+
+logger = logging.getLogger(__name__)
 
 
-class RandomProcess(Component):
+class GetPixelValues(Component):
     implements(ProcessInterface)
 
     identifier = "getdata"
-    title = "Retrieves value at requested point"
+    title = "Pixel Value extractor"
     description = "Creates a string output in csv style with the defined number of random generated values."
     metadata = ["a", "b"]
     profiles = ["p", "q"]
@@ -219,3 +226,176 @@ class GetTimeDataProcess(Component):
 
         return output.getvalue()
         
+
+
+class GetCoverageDifference(Component):
+    implements(ProcessInterface)
+
+    identifier = "getCoverageDifference"
+    title = "Difference image computation"
+    description = "Creates difference of two coverages"
+    metadata = ["a", "b"]
+    profiles = ["p", "q"]
+
+    inputs = {
+        "master_id": str,
+        "slave_id": str,
+        "bbox": str,
+        "crs": int
+    }
+
+    outputs = {
+        "processed": str
+    }
+
+    def execute(self, master_id, slave_id, bbox, crs):
+        """ The main execution function for the process.
+        """
+    
+        master = models.RectifiedDataset.objects.get(identifier=master_id)
+        slave = models.RectifiedDataset.objects.get(identifier=slave_id)
+
+        filename_master = connect(master.data_items.get(semantic__startswith="bands"))
+        filename_slave = connect(slave.data_items.get(semantic__startswith="bands"))
+
+        ds_master = gdal.Open(filename_master)
+        ds_slave = gdal.Open(filename_slave)
+
+
+        # bbox in pixels 
+        
+        rm = Rect(0,0,ds_master.RasterXSize,ds_master.RasterYSize)
+        rs = Rect(0,0,ds_slave.RasterXSize,ds_slave.RasterYSize)
+
+        ext = rm & rs 
+
+        pix_master = np.array(ds_master.ReadAsArray(ext.offset_x,ext.offset_y,ext.size_x,ext.size_y),'float32')
+        pix_slave  = np.array(ds_slave.ReadAsArray(ext.offset_x,ext.offset_y,ext.size_x,ext.size_y),'float32')
+
+        #pix_res    = pix_master - pix_slave
+    
+        def _diff(a,b):
+            c = np.zeros((a.shape[1],a.shape[2]))
+            for i in xrange(a.shape[0]):
+                c[:,:] += ( a[i,:,:] - b[i,:,:] )**2
+            return np.sqrt(c)
+
+        pix_res = _diff(pix_master, pix_slave)
+
+        max_pix = np.max(pix_res)
+        scale = 254.0/max_pix if max_pix > 0 else 1.0
+
+        pix_res = np.array(pix_res*scale+1,'uint8')
+
+        print "%s: pix_res.shape=%s"%(self.identifier,repr(pix_res.shape))
+
+        # the output image
+
+        driver = gdal.GetDriverByName('GTiff')
+
+        filename = "/tmp/%s_%s.tif" %( self.identifier,uuid4().hex )
+
+        try:
+         
+            ds_res = driver.Create(filename,ext.size_x,ext.size_y,1,gdal.GDT_Byte)
+
+            ds_res.GetRasterBand(1).WriteArray(pix_res,0,0)
+
+            with open(filename) as f:
+                output = f.read()
+
+        except Exception as e: 
+
+            if os.path.isfile(filename):
+                os.remove(filename)
+
+            raise e
+           
+        else: 
+            pass
+            #os.remove(filename)
+        
+
+        #maxmax = max(max(0,np.max(pix_res),abs(min(0,np.min(pix_res)))))
+        #scale = 1.0/maxmax if maxmax > 0 else 1.0 
+        #pix_res = np.array(127.0*(pix_res*scale+1)+1,'uint8')
+
+
+        return {
+            "processed": output
+        }
+
+
+
+
+
+
+        '''
+
+        bbox_p = bbox.split(',')
+
+        polygon = Polygon((bbox_p[0],bbox_p[1]), (bbox_p[0],bbox_p[0]), (bbox_p[0],bbox_p[0]), (bbox_p[0],bbox_p[0]))
+
+        points = []
+        for coordinate in coordinates:
+            x,y = coordinate.split(',')
+            # parameter parsing
+            point = Point(float(x), float(y))
+            point.srid = srid
+            points.append(point)
+
+        points = MultiPoint(points)
+        points.srid = srid
+
+
+        eo_objects = eo_objects.filter(
+            footprint__intersects=points
+        )
+
+        output = StringIO()
+        writer = csv.writer(output, quoting=csv.QUOTE_ALL)
+        #header = ["id", "begin", "end"] + ["point%d" % i for i in range(len(points))]
+        header = ["id", "Green", "Red", "NIR", "MIR" ]
+        writer.writerow(header)
+
+        for eo_object in eo_objects:
+            coverage = eo_object.cast()
+
+            #values = [coverage.identifier, isoformat(coverage.begin_time), isoformat(coverage.end_time)] + [None] * len(points)
+            values = [collection] + [None] * 4
+
+            data_item = coverage.data_items.get(semantic__startswith="bands")
+            filename = connect(data_item)
+            ds = gdal.Open(filename)
+            sr = SpatialReference(ds.GetProjection())
+            #points_t = points.transform(sr, clone=True)
+
+            for index, point in enumerate(points, start=1):
+
+                if not coverage.footprint.contains(point):
+                    continue
+
+                gt = ds.GetGeoTransform()
+
+                point.transform(sr)
+         
+                # Works only if gt[2] and gt[4] equal zero! 
+                px = int((point[0] - gt[0]) / gt[1]) #x pixel
+                py = int((point[1] - gt[3]) / gt[5]) #y pixel
+
+                #array = ds.ReadRaster(px, py, 1, 1)
+                #structval = ds.ReadRaster(px,py,1,1,buf_type=gdal.GDT_Int16) #TODO: Check Range Type to adapt buf_type!
+                pixelVal = ds.ReadAsArray(px,py,1,1)[:,0,0]
+
+                #pixel_value = array[0][0]
+                #print structval
+                #pixel_value = struct.unpack('IIII' , structval) #use the 'short' format code (2 bytes) not int (4 bytes)
+                
+                #values[index] = pixel_value[0]
+                #writer.writerow([ col_name+"_p"+str(index), pixelVal[0], pixelVal[1], pixelVal[2], pixelVal[3] ])
+                writer.writerow([ "P_"+str(index), pixelVal[0], pixelVal[1], pixelVal[2], pixelVal[3] ])
+
+        return {
+            "processed": output.getvalue()
+        }
+        '''
