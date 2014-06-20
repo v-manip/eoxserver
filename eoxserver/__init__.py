2b9315852ed99fbe352eb2814a1a8ce1e841fd78
Daniel Santillan <daniel.santillan@eox.at>
2014-06-20 18:29:05 +0200

    Adjusting version

--- eoxserver/__init__.py
+++ eoxserver/__init__.py
@@ -27,7 +27,7 @@
 # THE SOFTWARE.
 #-------------------------------------------------------------------------------
 
-VERSION = (0, 4, None, 'dev', 0)
+VERSION = (0, 4, 1, 'dev', 0)
 #VERSION = (0, 4, None, 'alpha', 1)
 #VERSION = (0, 4, None, 'beta', 1)
 #VERSION = (0, 4, None, 'rc', 1)
